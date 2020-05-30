a32ac2228c0b61b32e5343b6904a2716bb1308e3
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-05-29 21:35:28 -0500

    echo and prometeus

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	echoPrometheus "github.com/globocom/echo-prometheus"
+	"github.com/labstack/echo/v4"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(echoPrometheus.MetricsMiddleware())
+	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+
+	e.GET("/test", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
